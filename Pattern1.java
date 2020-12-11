ee146006431fa1674ddda32653545ee41bd44fb1
kashi0384 <75771833+kashi0384@users.noreply.github.com>
2020-12-11 11:49:36 +0530

    Patternexample

---
+++ Pattern1.java
@@ -0,0 +1,17 @@
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5,num;
+		for(int i=0;i<n;i++) {
+			num=5;
+			for(int j=0;j<=i;j++) {
+				System.out.print(num+" ");
+				num--;
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ Pattern2.java
@@ -0,0 +1,18 @@
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			for(int j=0;j<=i;j++) {
+			System.out.print(" ");
+			}
+			for(int j=n; j>=i; j--) {
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+	}
+
+}

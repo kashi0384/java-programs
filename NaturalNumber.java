61b8b67accb764ea29af0907f10a1c5223ab1080
kashi0384 <75771833+kashi0384@users.noreply.github.com>
2020-12-11 11:47:41 +0530

    Looping concept

---
+++ NaturalNumber.java
@@ -0,0 +1,14 @@
+
+public class NaturalNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, sum=0; 
+		
+		for(i=5;i<=25;i++) {
+		sum += i;
+		}
+		System.out.println("sum of natural numbers from 5 to 25 is:" + sum);
+	}
+
+}

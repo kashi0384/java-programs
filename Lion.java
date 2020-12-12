d85d862143158874e9b9446f75ed472040fc9da4
kashi0384 <75771833+kashi0384@users.noreply.github.com>
2020-12-12 19:50:55 +0530

    Method implementations

---
+++ Lion.java
@@ -0,0 +1,8 @@
+
+public class Lion {
+	static int lifespan;
+	static String breed;
+	static String color;
+	 static int weight;
+	
+}
---
+++ LionTester.java
@@ -0,0 +1,19 @@
+
+public class LionTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Lion list=new Lion();
+		Lion.color="orangebrown";
+		Lion.breed="Lion";
+		Lion.lifespan=14;
+		Lion.weight=50;
+		System.out.println("Detail of Lion:");
+		System.out.println("color of Lion:"+Lion.color);
+		System.out.println("Life span of Lion:"+Lion.lifespan);
+		System.out.println("weight of Lion in kg:"+Lion.weight);
+		
+		
+	}
+
+}
